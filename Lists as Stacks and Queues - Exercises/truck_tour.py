028c5ca9a860390b1980527013bae58d6426db72
ChesterLenSB <svetoslavbaykov55@gmail.com>
2023-01-14 20:38:29 +0200

    Solution for exercise 5 added

---
+++ Lists as Stacks and Queues - Exercises/truck_tour.py
@@ -0,0 +1,22 @@
+from collections import deque
+
+pumps_data = deque([[int(x) for x in input().split()] for _ in range(int(input()))])
+
+pumps_data_copy = pumps_data.copy()
+index = 0
+gas_in_tank = 0
+
+while pumps_data_copy:
+    petrol, distance = pumps_data_copy.popleft()
+
+    gas_in_tank += petrol
+
+    if gas_in_tank - distance < 0:
+        pumps_data.rotate(-1)
+        pumps_data_copy = pumps_data.copy()
+        index += 1
+        gas_in_tank = 0
+    else:
+        gas_in_tank -= distance
+
+print(index)
