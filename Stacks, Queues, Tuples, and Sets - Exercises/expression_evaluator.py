8570a64cf287545486e52cc255c5111d639c4e0a
ChesterLenSB <svetoslavbaykov55@gmail.com>
2023-01-19 03:32:52 +0200

    Solution for exercise 2 added

---
+++ Stacks, Queues, Tuples, and Sets - Exercises/expression_evaluator.py
@@ -0,0 +1,25 @@
+from functools import reduce
+
+expression = input().split()
+
+index = 0
+
+functions = {
+    "*": lambda i: reduce(lambda a, b: int(a) * int(b), expression[:i]),
+    "/": lambda i: reduce(lambda a, b: int(a) / int(b), expression[:i]),
+    "+": lambda i: reduce(lambda a, b: int(a) + int(b), expression[:i]),
+    "-": lambda i: reduce(lambda a, b: int(a) - int(b), expression[:i]),
+}
+
+while index < len(expression):
+    element = expression[index]
+
+    if element in "*/+-":
+        result = functions[element](index)
+        [expression.pop(1) for i in range(index)]
+        expression[0] = result
+        index = 0
+
+    index += 1
+
+print(int(expression[0]))
