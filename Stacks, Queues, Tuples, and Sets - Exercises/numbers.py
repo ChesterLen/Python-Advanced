1603a462bf0fd5d5689ba5acea88d0f63c2506ee
ChesterLenSB <svetoslavbaykov55@gmail.com>
2023-01-19 02:40:07 +0200

    Code for exercise 1 updated

--- Stacks, Queues, Tuples, and Sets - Exercises/numbers.py
+++ Stacks, Queues, Tuples, and Sets - Exercises/numbers.py
@@ -1,22 +1,22 @@
-first_sequence = set(int(x) for x in input().split())
-second_sequence = set(int(x) for x in input().split())
+first_set = set([int(x) for x in input().split()])
+second_set = set(int(x)for x in input().split())
+
+functions = {
+    "Add First": lambda x: [first_set.add(int(x)) for x in x],
+    "Add Second": lambda x: [second_set.add(int(x)) for x in x],
+    "Remove First": lambda x: [first_set.discard(int(x)) for x in x],
+    "Remove Second": lambda x: [second_set.discard(int(x)) for x in x],
+    "Check Subset": lambda: print(True) if first_set.issubset(second_set) or second_set.issubset(first_set) else print(False),
+}
 
 for _ in range(int(input())):
     first_command, *data = input().split()
     command = first_command + " " + data.pop(0)
-    if command == "Add First":
-        [first_sequence.add(int(number)) for number in data]
-    elif command == "Add Second":
-        [second_sequence.add(int(number)) for number in data]
-    elif command == "Remove First":
-        [first_sequence.discard(int(number)) for number in data]
-    elif command == "Remove Second":
-        [second_sequence.discard(int(number)) for number in data]
-    else:
-        if first_sequence.issubset(second_sequence) or second_sequence.issubset(first_sequence):
-            print(True)
-        else:
-            print(False)
 
-print(*sorted(first_sequence), sep=", ")
-print(*sorted(second_sequence), sep=", ")
+    if data:
+        functions[command]([int(x) for x in data])
+    else:
+        functions[command]()
+
+print(*sorted(first_set), sep=", ")
+print(*sorted(second_set), sep=", ")
