c50a0931fa7bdb2189286b8c67300668aab93584
ChesterLenSB <svetoslavbaykov55@gmail.com>
2023-01-21 00:45:00 +0200

    Solution for exercise 1 added

---
+++ Multidimensional Lists - Lab/sum_matrix_elements.py
@@ -0,0 +1,12 @@
+matrix_size = [int(x) for x in input().split(", ")]
+row, column = matrix_size
+matrix = []
+matrix_sum = 0
+
+for i in range(row):
+    row_data = list(map(int, input().split(", ")))
+    matrix.append(row_data)
+    matrix_sum += sum(row_data)
+
+print(matrix_sum)
+print(matrix)
