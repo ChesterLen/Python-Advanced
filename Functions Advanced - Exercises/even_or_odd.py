9de72dea1338c55d2ea1ce0b1ffe9ca638eec783
ChesterLenSB <svetoslavbaykov55@gmail.com>
2023-01-31 11:40:52 +0200

    Update even_or_odd.py

--- Functions Advanced - Exercises/even_or_odd.py
+++ Functions Advanced - Exercises/even_or_odd.py
@@ -5,6 +5,3 @@ def even_odd(*args):
     if command == "even":
         return list(num for num in list_args if num % 2 == 0)
     return list(num for num in list_args if num % 2 != 0)
-
-
-print(even_odd(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, "odd"))
