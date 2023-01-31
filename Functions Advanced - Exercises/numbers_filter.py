1d6b44125f1c28fd07ef8b27714c652f9988b845
ChesterLenSB <svetoslavbaykov55@gmail.com>
2023-01-31 11:30:40 +0200

    Solution for exercise 4 added

---
+++ Functions Advanced - Exercises/numbers_filter.py
@@ -0,0 +1,13 @@
+def even_odd_filter(**kwargs):
+    if "even" in kwargs:
+        kwargs["even"] = [x for x in kwargs["even"] if x % 2 == 0]
+
+    if "odd" in kwargs:
+        kwargs["odd"] = [x for x in kwargs["odd"] if x % 2 != 0]
+
+    return dict(sorted(kwargs.items(), key=lambda x: -len(x[1])))
+
+
+print(even_odd_filter(
+    odd=[2, 2, 30, 44, 10, 5],
+))
