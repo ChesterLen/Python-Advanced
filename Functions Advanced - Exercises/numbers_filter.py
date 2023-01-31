d20270e72ee5d458836daefe8e0ad99967cec5cb
ChesterLenSB <svetoslavbaykov55@gmail.com>
2023-01-31 11:43:08 +0200

    Update numbers_filter.py

--- Functions Advanced - Exercises/numbers_filter.py
+++ Functions Advanced - Exercises/numbers_filter.py
@@ -6,8 +6,3 @@ def even_odd_filter(**kwargs):
         kwargs["odd"] = [x for x in kwargs["odd"] if x % 2 != 0]
 
     return dict(sorted(kwargs.items(), key=lambda x: -len(x[1])))
-
-
-print(even_odd_filter(
-    odd=[2, 2, 30, 44, 10, 5],
-))
