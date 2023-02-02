234c16bea819b85e8a5fbcf7eb21f171491a3f17
Svetoslav Baykov <svetoslavbaykov55@gmail.com>
2023-02-02 03:50:21 -0800

    Create fill_the_box.py

---
+++ Functions Advanced - Exercises/fill_the_box.py
@@ -0,0 +1,20 @@
+def fill_the_box(height, length, width, *args):
+    volume = height * length * width
+    cubes = sum(x for x in args if x != 'Finish')
+    for el in args:
+        if el == 'Finish':
+            break
+        if el > volume:
+            difference = el - volume
+            volume -= (el - difference)
+            cubes -= (el - difference)
+        else:
+            volume -= el
+            cubes -= el
+    if volume:
+        return f"There is free space in the box. You could put {volume} more cubes."
+    else:
+        return f"No more free space! You have {cubes} more cubes."
+ 
+ 
+print(fill_the_box(2, 8, 2, 2, 1, 7, 3, 1, 5, "Finish"))
